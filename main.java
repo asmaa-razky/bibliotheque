46d473a57fddbf0c7a019ef1a2c7bfacb4534edb
asmaa-razky <160224008+asmaa-razky@users.noreply.github.com>
2024-02-19 11:52:10 +0100

    main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+	
+	public static void main(String[] args) {
+        Livre livre1 = new Livre("le dernier jour d'un condamné", "victor hugo", 1999);
+        System.out.println("Détails du livre:");
+        livre1.afficherDetails();
+    }
+	
+
+}
